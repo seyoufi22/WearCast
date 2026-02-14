f7f52f3ec1f30701bc6f91c0193c23bd63a38b0d
Omar <omarfawzy0025@gmail.com>
2026-02-14 17:42:47 +0200

    make ID Identity on BaseMode

--- WearCast.Api/Persistence/EntitiesConfigurations/BaseModelConfiguration.cs
+++ WearCast.Api/Persistence/EntitiesConfigurations/BaseModelConfiguration.cs
@@ -1,6 +1,18 @@
 ﻿namespace WearCast.Api.Persistence.EntitiesConfigurations
 {
-    public class BaseModelConfiguration
+    public class BaseModelConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+        where TEntity : BaseModel
     {
+        public void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            // 1. تحديد الـ Primary Key
+            builder.HasKey(e => e.ID);
+
+            // 2. إعداد الـ Guid Identity
+            builder.Property(e => e.ID)
+                   .HasDefaultValueSql("NEWID()")
+                   .ValueGeneratedOnAdd();
+
+        }
     }
 }
