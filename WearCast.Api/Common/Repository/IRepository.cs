257ea4717c1829b032fd488e9b5dcb94b4ff0c17
seyoufi22 <ahmedseyoufi009@gmail.com>
2026-02-14 08:49:04 +0200

    SetUp Repository and BaseModel

--- WearCast.Api/Common/Repository/IRepository.cs
+++ WearCast.Api/Common/Repository/IRepository.cs
@@ -1,6 +1,13 @@
+using System.Linq.Expressions;
+
 namespace WearCast.Api.Common.Repository;
 
-public interface IRepository
+public interface IRepository<T>
 {
-    
+    public Task<List<T>> GetAllAsync();
+    public Task<T> GetAsync(Expression<Func<T, bool>>filter,  bool useNoTracking = false);
+    public Task<T> CreateAsync(T dbRecord);
+    public Task<T> UpdateAsync(T dbRecord);
+    public Task HardDeleteAsync(T dbRecord);
+    public Task SoftDeleteAsync(Guid  entityId);
 }
