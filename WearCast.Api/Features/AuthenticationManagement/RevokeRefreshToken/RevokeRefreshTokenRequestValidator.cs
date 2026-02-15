9a8213e3edd553da6adf4fc668fd65ad4203c22d
Karim Hassib <karimhassib20@gmail.com>
2026-02-15 03:42:55 +0200

    Add RevokeRefreshToken Feature

---
+++ WearCast.Api/Features/AuthenticationManagement/RevokeRefreshToken/RevokeRefreshTokenRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace WearCast.Api.Features.AuthenticationManagement.RevokeRefreshToken
+{
+    public class RevokeRefreshTokenRequestValidator : AbstractValidator<RevokeRefreshTokenRequest>
+    {
+        public RevokeRefreshTokenRequestValidator() 
+        {
+            RuleFor(x => x.Token).NotEmpty();
+            RuleFor(x => x.RefreshToken).NotEmpty();
+        }
+    }
+}
