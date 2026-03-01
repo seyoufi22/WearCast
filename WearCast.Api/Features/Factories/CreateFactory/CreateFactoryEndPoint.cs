70be9d9ca598fbf6da0e290ec1b4b200f4bb8c0c
Karim Hassib <karimhassib20@gmail.com>
2026-03-01 02:56:27 +0200

    Add     [Consumes(multipart/form-data)] to some EndPoints

--- WearCast.Api/Features/Factories/CreateFactory/CreateFactoryEndPoint.cs
+++ WearCast.Api/Features/Factories/CreateFactory/CreateFactoryEndPoint.cs
@@ -7,6 +7,7 @@
         private readonly IMediator _mediator = mediator;
 
         [HttpPost]
+        [Consumes("multipart/form-data")]
         public async Task<IActionResult> Create([FromForm] CreateFactoryRequest request, CancellationToken cancellationToken)
         {
             var result = await _mediator.Send(request, cancellationToken);
