cd9747367cb4853fa16d6d39f279b02954e2b220
Karim Hassib <karimhassib20@gmail.com>
2026-03-15 15:11:00 +0200

    Seeding SuperAdmin

---
+++ WearCast.Api/Common/Consts/DefaultUsers.cs
@@ -0,0 +1,12 @@
+﻿namespace WearCast.Api.Common.Consts
+{
+    public static class DefaultUsers
+    {
+        public const string SuperAdminId = "fe83a0d2-cc41-4305-bcea-799fe7af0de2";
+        public const string SuperAdminEmail = "SuperAdmin@WearCast.com";
+        public const string SuperAdminPassword = "P@ssword123";
+        public const string SuperAdminSecurityStamp = "DAE8F8342FB84409A3CF6B6BE8802BC8";
+        public const string SuperAdminConcurrencyStamp = "5e7bb6ec-e063-4e6f-a929-1fbe81b3c4d0";
+
+    }
+}
