985af4c42209072bd7f7ac5bac0d953b17598714
Karim Hassib <karimhassib20@gmail.com>
2026-04-11 02:45:24 +0200

    Add GetAllFactoryProductsForFactory, GetAllFactoryProductsForCustomers and update GetFactoryProductDetails

---
+++ WearCast.Api/Features/DesignedProductManagement/CustomerCatalogAndWorkspace/GetAllFactoryProductsForCustomers/GetAllFactoryProductsForCustomersEndPoint.cs
@@ -0,0 +1,18 @@
+﻿namespace WearCast.Api.Features.DesignedProductManagement.CustomerCatalogAndWorkspace.GetAllFactoryProductsForCustomers
+{
+    [Route("api/customer/catalog/designed-products")]
+    [ApiController]
+    [Authorize(Roles = $"{DefaultRoles.Customer},{DefaultRoles.SuperAdmin}")]
+    [Tags("Designed Product Catalog")]
+    public class GetAllFactoryProductsForCustomersEndPoint(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllFactoryProductsForCustomersRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(request, cancellationToken);
+            return result.ToResponse();
+        }
+    }
+}
---
+++ WearCast.Api/Features/DesignedProductManagement/CustomerCatalogAndWorkspace/GetAllFactoryProductsForFactory/GetAllFactoryProductsForFactoryEndPoint.cs
@@ -0,0 +1,18 @@
+﻿namespace WearCast.Api.Features.DesignedProductManagement.CustomerCatalogAndWorkspace.GetAllFactoryProductsForFactory
+{
+    [Route("api/factories/catalog/designed-products")]
+    [ApiController]
+    [Authorize(Roles = $"{DefaultRoles.FactoryManager},{DefaultRoles.SuperAdmin}")]
+    [Tags("Designed Product Catalog")]
+    public class GetAllFactoryProductsForFactoryEndPoint(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllFactoryProductsForFactoryRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(request, cancellationToken);
+            return result.ToResponse();
+        }
+    }
+}
