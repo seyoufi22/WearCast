f71d987f8d24bfb70334cb3d727b93be790b9a17
Omar <omarfawzy0025@gmail.com>
2026-04-11 14:14:26 +0200

    Add Get Customer Profile

---
+++ WearCast.Api/Features/Customers/GetCustomer/GetCustomerResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WearCast.Api.Features.Customers.GetCustomer;
+
+public record GetCustomerResponse(
+    int Id,
+    string FirstName,
+    string LastName,
+    string PhoneNumber,
+    AddressDto Address
+);
