446528e99c4b5a7cd9d802d175229f0682cbf35d
seyoufi22 <ahmedseyoufi009@gmail.com>
2026-04-16 20:51:16 +0200

    Add GetOrderByShipmentId endpoint

---
+++ WearCast.Api/Features/Orders/GetOrdersByShipmentId/GetOrdersByShipmentIdEndpoint.cs
@@ -0,0 +1,37 @@
+using WearCast.Api.Common.Extensions;
+using WearCast.Api.Features.Orders.GetOrdersByShipmentId.Query;
+
+namespace WearCast.Api.Features.Orders.GetOrdersByShipmentId;
+
+[Route("api/Orders")]
+[ApiController]
+[Tags("Order")]
+public class GetOrdersByShipmentIdEndpoint : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public GetOrdersByShipmentIdEndpoint(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet("shipment/{shipmentId}")]
+    [Authorize(Roles = DefaultRoles.Customer)]
+    public async Task<IActionResult> Get([FromRoute] int shipmentId)
+    {
+        var customerId = User.GetCustomerId();
+
+        var request = new GetOrdersByShipmentIdQuery(shipmentId, customerId!.Value);
+        var result = await _sender.Send(request);
+
+        if (result.IsFailure)
+        {
+            if (result.Error.Code == "Shipments.Forbidden")
+                return Forbid();
+
+            return NotFound(result.Error);
+        }
+
+        return Ok(result.Value);
+    }
+}
