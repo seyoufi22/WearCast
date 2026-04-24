12a7d00e4b494f6460f0a374310369db24230f3b
Karim Hassib <karimhassib20@gmail.com>
2026-04-24 06:31:49 +0300

    Change Create and Update Super Admin to Create and Update Admin

--- WearCast.Api/Common/Consts/DefaultRoles.cs
+++ WearCast.Api/Common/Consts/DefaultRoles.cs
@@ -6,6 +6,23 @@
         public const string SuperAdminRoleId = "efc6a646-310b-4571-9b0e-bdfebe29f921";
         public const string SuperAdminRoleConcurrencyStamp = "ed345b9e-2f7a-4b76-8d25-476cd4cb10db";
 
+        public const string OperationsAdmin = nameof(OperationsAdmin);
+        public const string OperationsAdminRoleId = "8b1912a5-4dc8-4f24-850d-830238fba901";
+        public const string OperationsAdminRoleConcurrencyStamp = "e5d9c2e3-2e70-4f5a-a38f-a957b10284d7";
+
+        public const string VendorAdmin = nameof(VendorAdmin);
+        public const string VendorAdminRoleId = "4c2b97f0-0a14-4869-b541-bc2961e5ef0a";
+        public const string VendorAdminRoleConcurrencyStamp = "f19d273a-4a25-47e1-8812-70b3e5138f29";
+
+        public const string CatalogAdmin = nameof(CatalogAdmin);
+        public const string CatalogAdminRoleId = "3f92b7c4-8a15-4d29-9e81-2c7b5a193f4e";
+        public const string CatalogAdminRoleConcurrencyStamp = "b7e5c1a2-9d3f-482a-a1b4-5e9f8c2d7a31";
+
+        public const string CustomerServiceAdmin = nameof(CustomerServiceAdmin);
+        public const string CustomerServiceAdminRoleId = "d8a2c1f5-3e7b-491a-82d4-5c9f1b7a2e38";
+        public const string CustomerServiceAdminRoleConcurrencyStamp = "5d2e7a1b-9f3c-481a-a2b5-e6f9d8c7b1a2";
+
+
         public const string Customer = nameof(Customer);
         public const string CustomerRoleId = "91756452-0c83-4c86-8129-88698116ee37";
         public const string CustomerRoleConcurrencyStamp = "fe83a0d2-cc41-4305-bcea-799fe7af0de2";
