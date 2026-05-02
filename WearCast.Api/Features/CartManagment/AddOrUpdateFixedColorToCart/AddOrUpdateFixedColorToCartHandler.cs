26f704763e2825b5036b542b9c8475b6c92cfe25
Omar <omarfawzy0025@gmail.com>
2026-05-03 01:13:39 +0300

    mod cart managment

---
+++ WearCast.Api/Features/CartManagment/AddOrUpdateFixedColorToCart/AddOrUpdateFixedColorToCartHandler.cs
@@ -0,0 +1,90 @@
+﻿using WearCast.Api.Features.CartManagment.AddOrUpdateFixedColorToCart.DTOs;
+using static WearCast.Api.Entities.CartItem;
+namespace WearCast.Api.Features.CartManagment.AddOrUpdateFixedColorToCart;
+
+public class AddOrUpdateFixedColorToCartHandler(
+    IRepository<CartItem> _cartItemRepository,
+    IRepository<Entities.FixedProduct.FixedProductColor> _colorRepository,
+    ApplicationDbContext _dbContext)
+    : IRequestHandler<AddOrUpdateFixedColorToCartCommand, Result>
+{
+    public async Task<Result> Handle(AddOrUpdateFixedColorToCartCommand command, CancellationToken cancellationToken)
+    {
+        var color = await _colorRepository.Get()
+         .Include(c => c.Sizes)
+         .FirstOrDefaultAsync(c => c.Id == command.Request.ColorId && !c.IsDeleted && !c.Product.IsDeleted&& !c.Product.Seller.IsDeleted, cancellationToken);
+
+        if (color == null)
+            return Result.Failure(new Error("Color.NotFound", "The specified color does not exist.", 404));
+
+        var requestedSizes = command.Request.Sizes.Select(s => s.Size).ToList();
+        var availableSizes = color.Sizes.Select(s => s.Size).ToList();
+
+        var unavailableSizes = requestedSizes.Except(availableSizes).ToList();
+        if (unavailableSizes.Any())
+        {
+            var missingSizesStr = string.Join(", ", unavailableSizes);
+            return Result.Failure(new Error("Color.SizeUnavailable", $"The following sizes are not available for this product template: {missingSizesStr}", 400));
+        }
+
+        var cartItem = await _cartItemRepository.Get()
+        .Include(c => c.Sizes)
+        .FirstOrDefaultAsync(c =>
+            c.FixedColorId == command.Request.ColorId &&
+            c.CustomerId == command.CustomerId,
+            cancellationToken);
+
+        try
+        {
+            if (cartItem == null)
+            {
+                var validInitialSizes = command.Request.Sizes.Where(s => s.Quantity > 0).ToList();
+
+                if (!validInitialSizes.Any())
+                    return Result.Success();
+
+                cartItem = new CartItem
+                {
+                    CustomerId = command.CustomerId,
+                    FixedColorId = command.Request.ColorId
+                };
+
+                var updates = validInitialSizes.Select(req =>
+                {
+                    var stock = color.Sizes.First(s => s.Size == req.Size).Quantity;
+                    return new SizeUpdateDto(req.Size, req.Quantity, stock);
+                }).ToList();
+
+                cartItem.AddOrUpdateSizes(updates);
+
+                await _cartItemRepository.CreateAsync(cartItem);
+            }
+            else
+            {
+                var updates = command.Request.Sizes.Select(req =>
+                {
+                    var stock = color.Sizes.First(s => s.Size == req.Size).Quantity;
+                    return new SizeUpdateDto(req.Size, req.Quantity, stock);
+                }).ToList();
+
+                cartItem.AddOrUpdateSizes(updates);
+
+                if (!cartItem.Sizes.Any())
+                {
+                    await _cartItemRepository.HardDeleteAsync(cartItem);
+                }
+                else
+                {
+                    _dbContext.Entry(cartItem).State = EntityState.Modified;
+                    await _dbContext.SaveChangesAsync(cancellationToken);
+                }
+            }
+        }
+        catch (InvalidOperationException ex)
+        {
+            return Result.Failure(new Error("Cart.StockExceeded", ex.Message, 400));
+        }
+
+        return Result.Success();
+    }
+}
