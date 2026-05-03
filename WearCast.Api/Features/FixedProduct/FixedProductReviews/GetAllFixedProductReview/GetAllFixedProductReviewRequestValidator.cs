83355c92a31cc62ac54f657e50da8b83dd4d6c33
Omar <omarfawzy0025@gmail.com>
2026-05-04 00:36:26 +0300

    add review to fixed Product

---
+++ WearCast.Api/Features/FixedProduct/FixedProductReviews/GetAllFixedProductReview/GetAllFixedProductReviewRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace WearCast.Api.Features.FixedProduct.FixedProductReviews.GetAllFixedProductReview;
+
+public class GetAllFixedProductReviewRequestValidator : AbstractValidator<GetAllFixedProductReviewRequest>
+{
+    public GetAllFixedProductReviewRequestValidator()
+    {
+        RuleFor(x => x.FixedProductId)
+            .GreaterThan(0).WithMessage("Invalid Product Id");
+    }
+}
