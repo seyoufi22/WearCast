c82c7d0aae3819e70ef19935a471ed3dbe1504e6
Karim Hassib <karimhassib20@gmail.com>
2026-05-18 04:17:07 +0300

    Add New Designed Product Notification

---
+++ WearCast.Api/Features/DesignedProductManagement/FactoryProducts/CreateDesignedProduct/NewDesignedProductEvent.cs
@@ -0,0 +1,19 @@
+﻿using WearCast.Api.Common.Interfaces.Notifications;
+
+namespace WearCast.Api.Features.DesignedProductManagement.FactoryProducts.CreateDesignedProduct
+{
+    public record NewDesignedProductEvent(
+    List<string> RecipientIds,
+    int ProductId,
+    string ProductName,
+    string ProductType) : INotification, INotificationEvent
+    {
+        public string Title => "New Product Created";
+
+        public string Message => $"A new {ProductType} '{ProductName}' has been created";
+
+        public NotificationType NotificationType => NotificationType.NewProduct;
+
+        public int? UrlId => ProductId;
+    }
+}
