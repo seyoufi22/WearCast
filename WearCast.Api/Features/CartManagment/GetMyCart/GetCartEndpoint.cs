47a47cf0c74c2362bb4f07f9a4f59bbd31745984
Omar <omarfawzy0025@gmail.com>
2026-05-19 05:33:12 +0300

    remove comment from get my cart

--- WearCast.Api/Features/CartManagment/GetMyCart/GetCartEndpoint.cs
+++ WearCast.Api/Features/CartManagment/GetMyCart/GetCartEndpoint.cs
@@ -1,24 +1,24 @@
-﻿//using System.Security.Claims;
-//using WearCast.Api.Features.CartManagment.GetMyCart.DTOs;
+﻿using System.Security.Claims;
+using WearCast.Api.Features.CartManagment.GetMyCart.DTOs;
 
-//namespace WearCast.Api.Features.CartManagment.GetMyCart;
+namespace WearCast.Api.Features.CartManagment.GetMyCart;
 
-//[Tags("Cart")]
-//[Route("api/Cart")]
-//[ApiController]
-//public class GetCartEndpoint(ISender sender) : ControllerBase
-//{
-//    [Authorize(Roles = "Customer")]
-//    [HttpGet("GetMyCart")]
-//    public async Task<IActionResult> GetCart(CancellationToken cancellationToken)
-//    {
-//        var customerId = User.FindFirstValue("CustomerId");
-//        if (string.IsNullOrEmpty(customerId))
-//            return Unauthorized();
+[Tags("Cart")]
+[Route("api/Cart")]
+[ApiController]
+public class GetCartEndpoint(ISender sender) : ControllerBase
+{
+    [Authorize(Roles = "Customer")]
+    [HttpGet("GetMyCart")]
+    public async Task<IActionResult> GetCart(CancellationToken cancellationToken)
+    {
+        var customerId = User.FindFirstValue("CustomerId");
+        if (string.IsNullOrEmpty(customerId))
+            return Unauthorized();
 
-//        var query = new GetCartRequestDto(int.Parse(customerId));
-//        var result = await sender.Send(query, cancellationToken);
+        var query = new GetCartRequestDto(int.Parse(customerId));
+        var result = await sender.Send(query, cancellationToken);
 
-//        return Ok(result);
-//    }
-//}
+        return Ok(result);
+    }
+}
