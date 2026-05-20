9853e854752b39901deded2c5bad66ddf8916d0f
Karim Hassib <karimhassib20@gmail.com>
2026-05-20 21:05:24 +0300

    Merge confirm email and resend confirm email fir seller manager with the confirm email and resend confirma email for other rules

--- WearCast.Api/Features/AuthenticationManagement/ResendConfirmEmail/ResendConfirmEmailHandler.cs
+++ WearCast.Api/Features/AuthenticationManagement/ResendConfirmEmail/ResendConfirmEmailHandler.cs
@@ -1,58 +1,87 @@
 ﻿using System.Security.Cryptography;
+using WearCast.Api.Features.Sellers;
 
 namespace WearCast.Api.Features.AuthenticationManagement.ResendConfirmEmail
 {
     public class ResendConfirmEmailHandler(
         UserManager<ApplicationUser> userManager,
+        ApplicationDbContext context,
         ILogger<ResendConfirmEmailHandler> logger,
         EmailHelper emailHelper
         ) : IRequestHandler<ResendConfirmEmailRequest, Result<ResendConfirmEmailResponse>>
     {
         private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly ApplicationDbContext _context = context;
         private readonly ILogger<ResendConfirmEmailHandler> _logger = logger;
         private readonly EmailHelper _emailHelper = emailHelper;
 
         public async Task<Result<ResendConfirmEmailResponse>> Handle(ResendConfirmEmailRequest request, CancellationToken cancellationToken)
         {
-            if (await _userManager.FindByEmailAsync(request.Email) is not { } user)
-                return Result.Success(new ResendConfirmEmailResponse(null!));
-
-            if (user.EmailConfirmed)
-                return Result.Failure<ResendConfirmEmailResponse>(UserErrors.DublicatedConfirmation);
-
             var code = RandomNumberGenerator.GetInt32(100000, 1000000).ToString();
-            user.EmailConfirmationCode = code;
-            user.EmailConfirmationCodeExpiration = DateTime.UtcNow.AddMinutes(60);
+            var expiration = DateTime.UtcNow.AddMinutes(60);
 
-            var updateResult = await _userManager.UpdateAsync(user);
-            if (!updateResult.Succeeded)
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user != null)
             {
-                _logger.LogError("Failed to update user {UserId} with new confirmation code.", user.Id);
-                return Result.Failure<ResendConfirmEmailResponse>(new("User.UpdateFailed", "Failed to process the request.", 500));
+                if (user.EmailConfirmed)
+                    return Result.Failure<ResendConfirmEmailResponse>(UserErrors.DublicatedConfirmation);
+
+                user.EmailConfirmationCode = code;
+                user.EmailConfirmationCodeExpiration = expiration;
+
+                var updateResult = await _userManager.UpdateAsync(user);
+                if (!updateResult.Succeeded)
+                {
+                    _logger.LogError("Failed to update user {UserId} with new confirmation code.", user.Id);
+                    return Result.Failure<ResendConfirmEmailResponse>(new("User.UpdateFailed", "Failed to process the request.", 500));
+                }
+
+                _logger.LogInformation("Resent Confirmation OTP: {code} for user {Email}", code, user.Email);
+
+                var roles = await _userManager.GetRolesAsync(user);
+                var primaryRole = roles.FirstOrDefault();
+
+                switch (primaryRole)
+                {
+                    case DefaultRoles.Driver:
+                        await _emailHelper.SendDriverConfirmationEmail(user, code);
+                        break;
+                    case DefaultRoles.FactoryManager:
+                        await _emailHelper.SendFactoryManagerConfirmationEmail(user, code);
+                        break;
+                    case DefaultRoles.ShippingCompanyManager:
+                        await _emailHelper.SendShippingCompanyManagerConfirmationEmail(user, code);
+                        break;
+                    case DefaultRoles.Customer:
+                        await _emailHelper.SendCustomerConfirmationEmail(user, code);
+                        break;
+                    case DefaultRoles.SellerManager:
+                        await _emailHelper.SendSellerManagerConfirmationEmail(user, code);
+                        break;
+                }
+
+                return Result.Success(new ResendConfirmEmailResponse(user.Id));
             }
 
-            _logger.LogInformation("Resent Confirmation OTP: {code} for user {Email}", code, user.Email);
+            var application = await _context.SellerApplications
+                .FirstOrDefaultAsync(x => x.ManagerEmail == request.Email, cancellationToken);
 
-            var roles = await _userManager.GetRolesAsync(user);
-            var primaryRole = roles.FirstOrDefault();
-
-            switch (primaryRole)
+            if (application != null)
             {
-                case DefaultRoles.Driver:
-                    await _emailHelper.SendDriverConfirmationEmail(user, code);
-                    break;
-                case DefaultRoles.FactoryManager:
-                    await _emailHelper.SendFactoryManagerConfirmationEmail(user, code);
-                    break;
-                case DefaultRoles.ShippingCompanyManager:
-                    await _emailHelper.SendShippingCompanyManagerConfirmationEmail(user, code);
-                    break;
-                case DefaultRoles.Customer:
-                    await _emailHelper.SendCustomerConfirmationEmail(user, code);
-                    break;
-            }
+                if (application.ManagerEmailConfirmed)
+                    return Result.Failure<ResendConfirmEmailResponse>(SellerApplicationErrors.DublicatedConfirmation);
 
-            return Result.Success(new ResendConfirmEmailResponse(user.Id));
+                application.ManagerEmailConfirmationCode = code;
+                application.ManagerEmailConfirmationCodeExpiration = expiration;
+
+                _context.SellerApplications.Update(application);
+                await _context.SaveChangesAsync(cancellationToken);
+
+                await _emailHelper.SendConfirmationEmailForSellerManager(application, code);
+
+                return Result.Success(new ResendConfirmEmailResponse(application.ManagerEmail));
+            }
+            return Result.Success(new ResendConfirmEmailResponse(null!));
         }
     }
 }
--- WearCast.Api/Features/Sellers/SellerApplications/SellerManagerConfirmEmail/SellerManagerConfirmEmailRequest.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace WearCast.Api.Features.Sellers.SellerApplications.SellerManagerConfirmEmail
-{
-    public record SellerManagerConfirmEmailRequest(string Email, string Code) : IRequest<Result>;
-}
